f18f23b7ded5a1fa42dfceebd392ce24583aecae
drew0ps <ad.marton@proton.me>
2025-02-21 19:01:40 +0100

    Finished project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,24 +7,49 @@
 //
 
 import UIKit
+import UIKit
+import AVFoundation
+var player: AVAudioPlayer!
 
 class ViewController: UIViewController {
-    let eggTimes = ["Soft": 5*60, "Medium": 7*60, "Hard": 12*60]
+    let eggTimes = ["Soft": 5*2, "Medium": 7*60, "Hard": 12*60]
     var counter = 30
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+
     
+    @IBOutlet weak var eggProgress: UIProgressView!
+
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
-        counter = eggTimes[hardness]!
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer.invalidate()
+        secondsPassed = 0
+        eggLabel.text = "How do you like your eggs?"
+        eggProgress.progress = 0.0
+        totalTime = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
 
-
-    @objc func updateCounter() {
-        //example functionality
-        if counter > 0 {
-            print("\(counter) seconds to the end of the world")
-            counter -= 1
-        }
-    }
+    @IBOutlet weak var eggLabel: UILabel!
     
+    @objc func updateCounter() {
+        if secondsPassed <= totalTime {
+
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            eggLabel.text = "\(round(percentageProgress * 100))%"
+            secondsPassed += 1
+            eggProgress.progress = Float(percentageProgress)
+        } else {
+            eggLabel.text = "Done!"
+            
+            func playSound(soundName: String) {
+                let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+                player = try! AVAudioPlayer(contentsOf: url!)
+                player.play()
+            }
+            playSound(soundName: "alarm_sound")
+
+            }
+        }
 }
